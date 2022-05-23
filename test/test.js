c070fb2dbd933583f721ba9d079f558548bc537b
Alex Van Liew <alexv@productiv.com>
2022-05-23 12:28:39 -0700

    don't duplicate test suite

--- test/test.js
+++ test/test.js
@@ -5,59 +5,60 @@ import plugin from '../src/plugin';
 
 const validHTML4id = /^[A-Za-z][A-Za-z0-9_:.-]*$/;
 
-describe('Vue 2', () => {
-  // Use a local Vue class to be able to also test plugin options
-  const Vue = createLocalVue();
-  Vue.use(plugin);
+function fillArray(fn, count = 1) {
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    result.push(fn());
+  }
+  return result;
+}
 
+function runPluginTests(mountVm) {
   describe('vm.uid', () => {
     it('exists', () => {
-      const vm = new Vue();
+      const vm = mountVm();
       assert.isOk(vm.uid);
       assert.isString(vm.uid);
     });
 
     it('is immutable', () => {
-      const vm = new Vue();
+      const vm = mountVm();
       assert.throws(() => { vm.uid = 'fail'; });
     });
 
     it('is unique', () => {
-      const vm1 = new Vue();
-      const vm2 = new Vue();
+      const [vm1, vm2] = mountVm(2);
       assert.notEqual(vm1.uid, vm2.uid);
     });
   });
 
   describe('vm.$id()', () => {
     it('exists', () => {
-      const vm = new Vue();
+      const vm = mountVm();
       assert.isOk(vm.$id);
       assert.isFunction(vm.$id);
     });
 
     describe('default id', () => {
       it('returns a valid HTML 4 id ', () => {
-        const vm = new Vue();
+        const vm = mountVm();
         assert.match(vm.$id(), validHTML4id);
       });
 
       it('is unique', () => {
-        const vm1 = new Vue();
-        const vm2 = new Vue();
+        const [vm1, vm2] = mountVm(2);
         assert.notEqual(vm1.$id(), vm2.$id());
       });
     });
 
     describe('scoped id', () => {
       it('returns a valid HTML 4 id ', () => {
-        const vm = new Vue();
+        const vm = mountVm();
         assert.match(vm.$id('scoped'), validHTML4id);
       });
 
       it('is unique', () => {
-        const vm1 = new Vue();
-        const vm2 = new Vue();
+        const [vm1, vm2] = mountVm(2);
         assert.notEqual(vm1.$id('scoped'), vm2.$id('scoped'));
       });
     });
@@ -65,13 +66,13 @@ describe('Vue 2', () => {
 
   describe('vm.$idRef()', () => {
     it('exists', () => {
-      const vm = new Vue();
+      const vm = mountVm();
       assert.isOk(vm.$idRef);
       assert.isFunction(vm.$idRef);
     });
 
     it('returns the id with "#" prepended', () => {
-      const vm = new Vue();
+      const vm = mountVm();
       assert.equal(vm.$idRef(), `#${vm.$id()}`);
     });
   });
@@ -81,11 +82,9 @@ describe('Vue 2', () => {
       uidProperty: 'my_uid',
       uidPrefix: 'custom-prefix-',
     };
-    const Vue = createLocalVue();
-    Vue.use(plugin, options);
 
     it('uidProperty', () => {
-      const vm = new Vue();
+      const vm = mountVm(options);
       const uid = vm[options.uidProperty];
       assert.isOk(uid);
       assert.isString(uid);
@@ -93,10 +92,26 @@ describe('Vue 2', () => {
     });
 
     it('uidPrefix', () => {
-      const vm = new Vue();
+      const vm = mountVm(options);
       assert.isOk(vm.$id().startsWith(options.uidPrefix));
     });
   });
+}
+
+describe('Vue 2', () => {
+  function mountVue2(count = 1, pluginOptions) {
+    if (typeof count === 'object') {
+      pluginOptions = count;
+      count = 1;
+    }
+
+    const Vue = createLocalVue();
+    Vue.use(plugin, pluginOptions);
+
+    return count === 1 ? new Vue() : fillArray(() => new Vue(), count);
+  }
+
+  runPluginTests(mountVue2);
 });
 
 describe('Vue 3', () => {
@@ -110,11 +125,7 @@ describe('Vue 3', () => {
     name: 'root',
     props: ['count'],
     render() {
-      const children = [];
-      for (let i = 0; i < (this.count || 1); i++) {
-        children.push(h(TestComponent));
-      }
-      return children;
+      return fillArray(() => h(TestComponent), this.count || 1);
     },
   };
 
@@ -129,102 +140,22 @@ describe('Vue 3', () => {
   });
 
   // these functions implement a simplified version of what VTU 2's `mount` and `findComponents` do
-  function mountVm(count = 1, options) {
+  function getTestComponents(vm) {
+    return vm.$.subTree.children.map((vnode) => vnode.component.proxy);
+  }
+
+  function mountVue3(count = 1, options) {
     if (typeof count === 'object') {
       options = count;
       count = 1;
     }
     app = createApp(RootComponent, { count });
     app.use(plugin, options);
-    // returns a vm for RootComponent. can be used without `getChildren()` for tests that only need
-    // a single component to run tests on
-    return app.mount(rootEl);
+    // returns a vm for RootComponent. can be used without `getTestComponents()`
+    // for tests that only need a single component to run tests on
+    const rootVm = app.mount(rootEl);
+    return count === 1 ? rootVm : getTestComponents(rootVm);
   }
 
-  function getTestComponents(vm) {
-    return vm.$.subTree.children.map((vnode) => vnode.component.proxy);
-  }
-
-  describe('vm.uid', () => {
-    it('exists', () => {
-      const vm = mountVm();
-      assert.isOk(vm.uid);
-      assert.isString(vm.uid);
-    });
-
-    it('is immutable', () => {
-      const vm = mountVm();
-      assert.throws(() => { vm.uid = 'fail'; });
-    });
-
-    it('is unique', () => {
-      const [vm1, vm2] = getTestComponents(mountVm(2));
-      assert.notEqual(vm1.uid, vm2.uid);
-    });
-  });
-
-  describe('vm.$id()', () => {
-    it('exists', () => {
-      const vm = mountVm();
-      assert.isOk(vm.$id);
-      assert.isFunction(vm.$id);
-    });
-
-    describe('default id', () => {
-      it('returns a valid HTML 4 id ', () => {
-        const vm = mountVm();
-        assert.match(vm.$id(), validHTML4id);
-      });
-
-      it('is unique', () => {
-        const [vm1, vm2] = getTestComponents(mountVm(2));
-        assert.notEqual(vm1.$id(), vm2.$id());
-      });
-    });
-
-    describe('scoped id', () => {
-      it('returns a valid HTML 4 id ', () => {
-        const vm = mountVm();
-        assert.match(vm.$id('scoped'), validHTML4id);
-      });
-
-      it('is unique', () => {
-        const [vm1, vm2] = getTestComponents(mountVm(2));
-        assert.notEqual(vm1.$id('scoped'), vm2.$id('scoped'));
-      });
-    });
-  });
-
-  describe('vm.$idRef()', () => {
-    it('exists', () => {
-      const vm = mountVm();
-      assert.isOk(vm.$idRef);
-      assert.isFunction(vm.$idRef);
-    });
-
-    it('returns the id with "#" prepended', () => {
-      const vm = mountVm();
-      assert.equal(vm.$idRef(), `#${vm.$id()}`);
-    });
-  });
-
-  describe('Options', () => {
-    const options = {
-      uidProperty: 'my_uid',
-      uidPrefix: 'custom-prefix-',
-    };
-
-    it('uidProperty', () => {
-      const vm = mountVm(options);
-      const uid = vm[options.uidProperty];
-      assert.isOk(uid);
-      assert.isString(uid);
-      assert.include(vm.$id(), uid);
-    });
-
-    it('uidPrefix', () => {
-      const vm = mountVm(options);
-      assert.isOk(vm.$id().startsWith(options.uidPrefix));
-    });
-  });
+  runPluginTests(mountVue3);
 });
